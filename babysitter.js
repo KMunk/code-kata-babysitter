275fa654ae3f4ad77177a980f0413d2f6120e56d
KMunk <kmunk@cloud-cme.com>
2021-06-23 12:54:17 -0400

    refactoring and adding constants for feature requirements

--- babysitter.js
+++ babysitter.js
@@ -4,25 +4,27 @@ As a babysitter
 In order to get paid for 1 night of work
 I want to calculate my nightly charge
 */
+const preBedTimeHourlyRate = 12;
+const earliestStartHour = 17;
+const latestEndHour = 4;
 
 module.exports = {
     isValidWorkingTime(workingTime) {
         let isValid = false;
 
         if(workingTime instanceof Date){
-            isValid = workingTime.getHours() >= 17 || workingTime.getHours() <= 4;
+            isValid = workingTime.getHours() >= earliestStartHour || workingTime.getHours() <= latestEndHour;
         }
         
         return isValid;
     },
     calculatePay(workingShift) {
         let preBedTimePay = 0;
-        console.log(workingShift);
+        
         if(this.isValidWorkingTime(workingShift.startTime) && this.isValidWorkingTime(workingShift.bedTime)){
             let timeDifference = Math.abs(workingShift.bedTime - workingShift.startTime);
             let hours = Math.floor(timeDifference / 3600000) % 24;
-            preBedTimePay = hours * 12;
-            console.log(hours);
+            preBedTimePay = hours * preBedTimeHourlyRate;
         }
 
         return {
